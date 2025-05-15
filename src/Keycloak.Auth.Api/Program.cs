da59da97c383ae50e0d1640e25887bdf38503b7c
SilverX21 <nuno21araujo95@gmail.com>
2025-05-15 21:42:12 +0100

    Add Keycloak proxy service and update configurations
    - Implement `keycloak.auth.proxy` service in Docker Compose files.
    - Enhance `Keycloak.Auth.Api.csproj` with OpenTelemetry and JWT Bearer packages.
    - Update `Program.cs` for reverse proxy, authentication, and new user claims endpoint.
    - Configure Keycloak URLs in `appsettings` files for proper authentication flow.

--- src/Keycloak.Auth.Api/Program.cs
+++ src/Keycloak.Auth.Api/Program.cs
@@ -1,9 +1,40 @@
+using System.Security.Claims;
 using Keycloak.Auth.Api.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGenWithAuth(builder.Configuration);
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(o =>
+    {
+        o.RequireHttpsMetadata = false;
+        o.Audience = builder.Configuration["Authentication:Audience"];
+        o.MetadataAddress = builder.Configuration["Authentication:MetadataAddress"]!;
+        o.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidIssuer = builder.Configuration["Authentication:ValidIssuer"],
+        };
+    });
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService("Keycloak.Auth.Proxy"))
+    .WithTracing(tracing =>
+    {
+        tracing.AddAspNetCoreInstrumentation()
+                    .AddHttpClientInstrumentation();
+
+        tracing.AddOtlpExporter();
+    });
 
 WebApplication app = builder.Build();
 
@@ -12,6 +43,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapGet("users/me", (ClaimsPrincipal claimsPrincipal) =>
+{
+    return claimsPrincipal.Claims.ToDictionary(c => c.Type, c => c.Value);
+}).RequireAuthorization();
 
 app.UseAuthentication();
 
